ec90ae071a7f674348d37ce9c6a2e57400c97100
FranMaric <fran.maric.vk@gmail.com>
2023-02-26 12:05:34 +0100

    Refactor

--- lib/clipped_fluid_page.dart
+++ lib/clipped_fluid_page.dart
@@ -12,6 +12,9 @@ const springStiffness = 6;
 const dampingStiffness = 2;
 const wallAttractionForce = 70.0;
 
+const _fps = 60;
+const _pointCount = 10;
+
 class ClippedFluidPage extends StatefulWidget {
   const ClippedFluidPage({required this.child, required this.size, Key? key}) : super(key: key);
 
@@ -26,36 +29,94 @@ class _ClippedFluidPageState extends State<ClippedFluidPage> {
   late final Timer timer;
   late final List<PhysicalPoint> points;
 
-  static const int fps = 60;
-  static const int pointCount = 10;
-
+  /// When gesture is considered completed isFlipped is set to true to move all points to the left
   bool isFlipped = false;
+
+  Offset? touchOffset;
+
+  /// Index of point on which force is applied from the drag gesture
+  int? pullingPointIndex;
 
   @override
   void initState() {
     super.initState();
 
     points = List.generate(
-      pointCount,
+      _pointCount,
       (index) => PhysicalPoint(
         position: Offset(
           widget.size.width,
-          widget.size.height / (pointCount - 1) * index,
+          widget.size.height / (_pointCount - 1) * index,
         ),
         velocity: Offset.zero,
         force: Offset.zero,
       ),
     );
 
-    timer = Timer.periodic(Duration(milliseconds: (1 / fps * 1000).round()), (timer) {
+    timer = Timer.periodic(Duration(milliseconds: (1 / _fps * 1000).round()), (timer) {
       calculatePositions();
       setState(() {});
     });
   }
 
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: onHorizontalDragStart,
+      onHorizontalDragUpdate: onHorizontalDragUpdate,
+      onHorizontalDragEnd: onHorizontalDragEnd,
+      child: Stack(
+        children: [
+          /// Only for debugging
+          Positioned.fill(
+            child: CustomPaint(
+              painter: DebugPainter(points, touchOffset),
+            ),
+          ),
+          Positioned.fill(
+            child: ClipPath(
+              clipper: FluidPageClipper(points.map((e) => e.position).toList()),
+              child: widget.child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+extension GestureCallbacks on _ClippedFluidPageState {
+  void onHorizontalDragStart(DragStartDetails details) {
+    if (details.localPosition.dx > widget.size.width - 50) {
+      pullingPointIndex = (details.globalPosition.dy * (_pointCount - 1) / widget.size.height).round();
+      touchOffset = details.globalPosition;
+    }
+  }
+
+  void onHorizontalDragUpdate(DragUpdateDetails details) {
+    touchOffset = details.globalPosition;
+
+    if (touchOffset!.dx < widget.size.width * 0.25) {
+      isFlipped = true;
+    }
+  }
+
+  void onHorizontalDragEnd(DragEndDetails details) {
+    touchOffset = null;
+    pullingPointIndex = null;
+  }
+}
+
+extension PhysicsAnimations on _ClippedFluidPageState {
   void calculatePositions() {
-    const dt = 1 / fps;
-    final restLength = widget.size.height / (pointCount - 1);
+    const dt = 1 / _fps;
+    final restLength = widget.size.height / (_pointCount - 1);
 
     for (int i = 0; i < points.length; i++) {
       points[i].force = Offset.zero;
@@ -130,8 +191,8 @@ class _ClippedFluidPageState extends State<ClippedFluidPage> {
 
       /// Check if it's at the right (initial) height
       if (points[i].position.dx == widget.size.width &&
-          points[i].position.dy != widget.size.height / (pointCount - 1) * i) {
-        points[i].position = Offset(widget.size.width, widget.size.height / (pointCount - 1) * i);
+          points[i].position.dy != widget.size.height / (_pointCount - 1) * i) {
+        points[i].position = Offset(widget.size.width, widget.size.height / (_pointCount - 1) * i);
       }
     }
   }
@@ -139,63 +200,13 @@ class _ClippedFluidPageState extends State<ClippedFluidPage> {
   double distanceBetween(Offset p1, Offset p2) {
     return pow(pow(p1.dx - p2.dx, 2) + pow(p1.dy - p2.dy, 2), 0.5).roundToDouble();
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onHorizontalDragStart: onHorizontalDragStart,
-      onHorizontalDragUpdate: onHorizontalDragUpdate,
-      onHorizontalDragEnd: onHorizontalDragEnd,
-      child: Stack(
-        children: [
-          /// Only for debugging
-          Positioned.fill(
-            child: CustomPaint(
-              painter: MyPainter(points, touchOffset),
-            ),
-          ),
-          Positioned.fill(
-            child: ClipPath(
-              clipper: FluidPageClipper(points.map((e) => e.position).toList()),
-              child: widget.child,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Offset? touchOffset;
-
-  /// Index of point on which force is applied from the drag gesture
-  int? pullingPointIndex;
-
-  void onHorizontalDragStart(DragStartDetails details) {
-    if (details.localPosition.dx > widget.size.width - 50) {
-      pullingPointIndex = (details.globalPosition.dy * (pointCount - 1) / widget.size.height).round();
-      touchOffset = details.globalPosition;
-    }
-  }
-
-  void onHorizontalDragUpdate(DragUpdateDetails details) {
-    touchOffset = details.globalPosition;
-
-    if (touchOffset!.dx < widget.size.width * 0.25) {
-      isFlipped = true;
-    }
-  }
-
-  void onHorizontalDragEnd(DragEndDetails details) {
-    touchOffset = null;
-    pullingPointIndex = null;
-  }
 }
 
 /// Used to paint positions and forces
 ///
 /// Only used for debugging
-class MyPainter extends CustomPainter {
-  MyPainter(this.points, this.touchOffset);
+class DebugPainter extends CustomPainter {
+  DebugPainter(this.points, this.touchOffset);
 
   final List<PhysicalPoint> points;
   final Offset? touchOffset;
